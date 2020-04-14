09bafddfb715d511d869a2d4444dde53eac46f0c
wjonke <wesjonke@gmail.com>
2020-04-13 21:52:45 -0400

    added skill images, attempted to import into skill card, doesnt like it, wont render picture. may need to put them in backend via cloudinary

--- src/components/home/SkillCard.js
+++ src/components/home/SkillCard.js
@@ -1,18 +1,14 @@
 import React from "react";
-
+import JS from "../../images/JS.jpg";
 const SkillCard = (props) => {
+  console.log(props.skill);
   return (
     <div className="card ">
-      <div className="card-image">
-        <img src="images/sample-1.jpg" alt="test pic" />
+      <div className="card-image" style={{ background: "black" }}>
+        <img src={props.skill.img} alt="Skill img" />
       </div>
       <div className="card-content center">
-        <p className="left-align">
-          Sample Skill, includes:
-          <li>picture</li>
-          <li> title</li>
-          <li>Content: {props.skill.entry}</li>
-        </p>
+        <p>{props.skill.title}</p>
       </div>
     </div>
   );
