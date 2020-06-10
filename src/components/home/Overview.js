849c22873d4c6a390fdaf65a3b60f621ab336e17
wjonke <wesjonke@gmail.com>
2020-06-09 22:33:03 -0400

    grammer

--- src/components/home/Overview.js
+++ src/components/home/Overview.js
@@ -5,10 +5,10 @@ const Overview = () => {
   return (
     <div className=" center-align">
       <P style={{ paddingLeft: "3em", paddingRight: "3em" }}>
-        I am a Full-stack Web Developer with experience using Javascript,
-        Python, HTML, CSS, React, Node.js with Express, algorithms, data
+        I am a Full-stack Web Developer with experience using JavaScript,
+        Python, HTML, CSS, React, NodeJS with Express, algorithms, data
         structures and testing. Previously, I was a Senior Project Manager for a
-        Low Voltage and Security company. I also carry a GA Low Voltage license.
+        low-voltage security company. I also carry a GA low-voltage license.
       </P>
     </div>
   );
